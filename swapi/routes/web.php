db83dd1f4405bfb6ff306e9bb32b105802cdd237
Qiang <qiangruizhangcheng@gmail.com>
2023-04-28 14:52:59 +0200

    cambio en la ruta raiz de laravel

--- swapi/routes/web.php
+++ swapi/routes/web.php
@@ -17,11 +17,11 @@ use GuzzleHttp\Client;
 Route::get('/', function () {
 $client = new Client([
     // Base URI is used with relative requests
-    'base_uri' => 'https://jsonplaceholder.typicode.com/',
+    'base_uri' => 'https://swapi.dev/api/',
     // You can set any number of default request options.
     'timeout'  => 2.0,
 ]);
-    $response = $client->request('GET', 'users');
+    $response = $client->request('GET', 'people');
     return json_decode($response->getBody()->getContents());
     
     return view('welcome');
