e24a64ee0a9e78b0973800550884dee6d02b7217
Qiang <qiangruizhangcheng@gmail.com>
2023-05-09 10:54:05 +0200

    cambios de la vista index y agregacion de nuevas funciones y estilos

--- AngularJS/app.js
+++ AngularJS/app.js
@@ -1,71 +1,90 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('PilotController', function($scope, $http) {
+myApp.controller('PilotController', function ($scope, $http) {
 
     // Obtenemos la lista completa de pilotos
-    $http.get('http://localhost/api/pilot').then(function(response) {
+    $http.get('http://localhost/api/pilot').then(function (response) {
         $scope.pilots = response.data;
-    }, function(error) {
+    }, function (error) {
         console.log(error);
     });
 
     // Función para eliminar un piloto
-    $scope.deletePilot = function(pilot) {
-        $http.delete('http://localhost/api/pilot/' + pilot.id).then(function(response) {
+    $scope.deletePilot = function (pilot) {
+        $http.delete('http://localhost/api/pilot/' + pilot.id).then(function (response) {
             // Eliminamos el piloto de la lista localmente
             var index = $scope.pilots.indexOf(pilot);
             if (index !== -1) {
                 $scope.pilots.splice(index, 1);
             }
-        }, function(error) {
+        }, function (error) {
             console.log(error);
         });
     };
 
+
+
 });
 
-myApp.controller('StarshipController', function($scope, $http) {
+myApp.controller('StarshipController', function ($scope, $http) {
 
     // Obtenemos la lista completa de naves
-    $http.get('http://localhost/api/starship').then(function(response) {
+    $http.get('http://localhost/api/starship').then(function (response) {
         $scope.starships = response.data;
-    }, function(error) {
+    }, function (error) {
         console.log(error);
     });
+    
+    //Función para mostrar una nave por su id
+    $scope.showStarship = function (starship) {
+        $http.get('http://localhost/api/starship/' + starship.id).then(function (response) {
+            $scope.sNave = response.data
+            console.log($scope.sNave)
+        }, function (error) {
+            console.log(error);
+        });
+    };
 
     // Función para eliminar una nave
-    $scope.deleteStarship = function(starship) {
-        $http.delete('http://localhost/api/starship/' + starship.id).then(function(response) {
+    $scope.deleteStarship = function (starship) {
+        $http.delete('http://localhost/api/starship/' + starship.id).then(function (response) {
             // Eliminamos la nave de la lista localmente
             var index = $scope.starships.indexOf(starship);
             if (index !== -1) {
                 $scope.starships.splice(index, 1);
             }
-        }, function(error) {
+        }, function (error) {
             console.log(error);
         });
     };
 
     // Función para obtener los pilotos asignados a una nave
     $scope.getPilotShip = function (starship) {
-        $http.get('http://localhost/api/pilotShip/' + starship.id).then(function(response) {
+        $http.get('http://localhost/api/starship/' + starship.id).then(function (response) {
+            $scope.sNave = response.data
+            console.log($scope.sNave)
+        }, function (error) {
+            console.log(error);
+        });
+    $http.get('http://localhost/api/pilotShip/' + starship.id).then(function (response) {
         $scope.pilotShips = response.data;
         console.log($scope.pilotShips)
-    }, function(error) {
+    }, function (error) {
         console.log(error);
     });
-    };
+};
 
-    // Función para eliminar la relacion de un piloto y una nave
-    $scope.deletePilotShip = function (pilotShip) {
-        $http.delete('http://localhost/api/pilotShip/' + pilotShip.id).then(function(response) {
-            // Eliminamos la nave de la lista localmente
-            var index = $scope.pilotShips.indexOf(pilotShip);
-            if (index !== -1) {
-                $scope.pilotShips.splice(index, 1);
-            }
-    }, function(error) {
+// Función para eliminar la relacion de un piloto y una nave
+$scope.deletePilotShip = function (pilotShip) {
+    $http.delete('http://localhost/api/pilotShip/' + pilotShip.id).then(function (response) {
+        // Eliminamos la nave de la lista localmente
+        var index = $scope.pilotShips.indexOf(pilotShip);
+        if (index !== -1) {
+            $scope.pilotShips.splice(index, 1);
+        }
+    }, function (error) {
         console.log(error);
     });
-    };
+};
+
 });
